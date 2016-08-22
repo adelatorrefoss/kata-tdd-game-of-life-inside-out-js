dd528eb4757d897d9ffeaa41c11898b2aa8ae071
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2016-08-22 16:17:55 +0200

    RED: one cell alone should die

--- test/gol.spec.js
+++ test/gol.spec.js
@@ -1,8 +1,13 @@
 // Game of Life
 
+var emptyGrid =[[0,0,0],[0,0,0],[0,0,0]];
+
 // production code
 function gol(seed) {
-  return [[0,0,0],[0,0,0],[0,0,0]];
+  return emptyGrid;
+}
+
+function lives(iAmAlive, neighbours) {
 }
 
 // tests
@@ -12,10 +17,40 @@ describe("A dead seed", function() {
   // afterEach(function() { });
 
   it('returns a dead grid as well', function() {
-    const emptyGrid =[[0,0,0],[0,0,0],[0,0,0]];
     const seed = emptyGrid;
     const next = gol(seed);
 
     expect(next).to.be.deep.equal(emptyGrid);
   });
 });
+
+
+// TODO: no RED
+
+// describe("A single live cell", function() {
+//   // beforeEach(function() { });
+//   // afterEach(function() { });
+
+//   it('should die', function() {
+//     const seed =[[0,0,0],[0,1,0],[0,0,0]];
+//     const next = gol(seed);
+
+//     expect(next).to.be.deep.equal(emptyGrid);
+//   });
+// })
+
+
+
+describe("A live cell", function() {
+  // beforeEach(function() { });
+  // afterEach(function() { });
+
+  it('should die by under population', function() {
+    const iAmAlive = true
+    const neighbours =[0,0,0, 0, 0, 0,0,0];
+
+    const nextIAmAlive = lives(iAmAlive, neighbours);
+
+    expect(nextIAmAlive).to.be.false;
+  });
+})
