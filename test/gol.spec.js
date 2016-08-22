4f9617ce8c87fc6e1530e170755108296f8331b0
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2016-08-22 14:22:35 +0200

    RED: A dead seed returns a dead grid

---
+++ test/gol.spec.js
@@ -0,0 +1,16 @@
+function gol(seed) {
+  return false;
+}
+
+describe("A dead seed", function() {
+  // beforeEach(function() { });
+  // afterEach(function() { });
+
+  it('returns a dead grid as well', function() {
+    const emptyGrid = [[0,0,0],[0,0,0],[0,0,0]];
+    const seed = [[0,0,0],[0,0,0],[0,0,0]];
+    const next = gol(seed);
+
+    expect(next).to.be.deep.equal(emptyGrid);
+  });
+});
