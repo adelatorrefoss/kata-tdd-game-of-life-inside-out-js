eff1c9fe6bd5b7bf72946c009b8c404ad953e563
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2016-08-24 12:36:06 +0200

    REFACTOR: move prod code to another file

--- test/gol.spec.js
+++ test/gol.spec.js
@@ -2,73 +2,6 @@
 
 chai.config.truncateThreshold = 0;
 
-
-const emptyGrid = [[false, false, false],
-                   [false, false, false],
-                   [false, false, false]];
-
-// production code
-
-function countAlive(neighbours) {
-  return neighbours.filter(x => x).length;
-}
-
-function lives(iAmAlive, numAlive) {
-  let cellLives = false;
-
-  if (iAmAlive) {
-    if (numAlive >= 2 && numAlive <= 3) {
-      cellLives = true;
-    }
-  } else if (numAlive === 3) {
-    cellLives = true;
-  }
-
-  return cellLives;
-}
-
-function getNeighbours(i, j, grid) {
-  const numRows = grid.length;
-  const numCols = grid[0].length;
-
-  const isValidPosition = (x, y) => {
-    if (x >= 0 && x < numRows && y >= 0 && y < numCols) {
-      return true;
-    }
-    return false;
-  };
-
-  const neighbours = [];
-  for (let m = -1; m <= 1; m++) {
-    for (let n = -1; n <= 1; n++) {
-      if (isValidPosition(i + m, j + n)) {
-        if (!(m === 0 && n === 0)) {
-          neighbours.push(grid[i + m][j + n]);
-        }
-      }
-    }
-  }
-  return neighbours;
-}
-
-function nextGol(seed) {
-  const next = [];
-
-  for (let i = 0; i < seed.length; i++) {
-    const nextRow = [];
-
-    for (let j = 0; j < seed[i].length; j++) {
-      const x = seed[i][j];
-      const neighbours = getNeighbours(i, j, seed);
-      const nextX = lives(x, countAlive(neighbours));
-      nextRow.push(nextX);
-    }
-
-    next.push(nextRow);
-  }
-  return next;
-}
-
 // tests
 
 describe('Given a single cell that is', () => {
@@ -242,6 +175,21 @@ describe('Given a grid that is', () => {
   });
 
   describe('a full grid', () => {
+    it('should evolve accordly', () => {
+      const seed = [[true, true, true],
+                    [true, true, true],
+                    [true, true, true]];
+      const next = nextGol(seed);
+
+      expect(next).to.be.deep.equal([[true, false, true],
+                                     [false, false, false],
+                                     [true, false, true]]);
+    });
+  });
+});
+
+describe('A evolution with', () => {
+  describe('a seed ', () => {
     it('should evolve accordly', () => {
       const seed = [[true, true, true],
                     [true, true, true],
