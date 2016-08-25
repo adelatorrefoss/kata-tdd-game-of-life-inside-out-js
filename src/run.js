daafe20172d04eb8ad31af648fc260e37a96e938
Antonio de la Torre <antondelatorre@gmail.com>
2016-08-25 17:34:00 +0200

    REFACTOR: fix runner

--- src/run.js
+++ src/run.js
@@ -1,20 +1,16 @@
+// Game Of Life Runner
+//
+// Example:
+// $ node src/run.js "[[true, true, true],[false,true, true],[false,true,true]]" true
+
 
 var g = require('../src/gol');
 
 const main = () => {
-  process.argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
+  const seed = JSON.parse(process.argv[2]);
+  const print = process.argv[3];
 
-    g.evolution([
-          [true, false, true, false, false, false],
-          [false, true, true, false, false, false],
-          [false, true, false, false, false, false],
-          [false, false, false, false, false, false],
-          [false, false, false, false, false, false],
-          [false, false, false, false, false, false],
-    ], true);
-
-  });
+  g.evolution(seed, print);
 };
 
 if (require.main === module) {
